d414a087706434f1260d3fc4bbed1673256df2dc
somethingsaid <kwong.billy@gmail.com>
2016-08-30 08:39:52 -0400

    Updated header.js and header.spec.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -16,7 +16,7 @@ class Header extends Component {
   render() {
     return (
       <header className="header">
-        <h1>todos</h1>
+        <h1>Yo!</h1>
         <TodoTextInput
           newTodo
           onSave={this.handleSave}
--- src/app/components/Header.spec.js
+++ src/app/components/Header.spec.js
@@ -30,7 +30,7 @@ describe('components', () => {
       const [h1, input] = output.props.children;
 
       expect(h1.type).toBe('h1');
-      expect(h1.props.children).toBe('todos');
+      expect(h1.props.children).toBe('Yo!');
 
       expect(input.type).toBe(TodoTextInput);
       expect(input.props.newTodo).toBe(true);
